0b1b5826dd9b9d2ba264748565940df0b8b71a46
SusaBe <bergter-susanne@web.de>
2022-11-02 14:22:38 +0100

    solution

---
+++ modules/index.js
@@ -0,0 +1,14 @@
+import {clickImages} from "./click.js"
+import {hoverHeader} from "./hover.js"
+import {scrollWindow} from "./scroll.js"
+
+
+
+clickImages()
+console.log(clickImages)
+
+hoverHeader()
+console.log(hoverHeader)
+
+scrollWindow()
+console.log(scrollWindow)
